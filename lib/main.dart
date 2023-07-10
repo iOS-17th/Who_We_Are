c7a4a72012b7d3809529e5bf4f883ec680391589
윤혁진 <gurwk574@naver.com>
2023-07-10 19:32:17 +0900

    feature_hello: start code

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'asdasd',
+      title: '안녕하세요',
       theme: ThemeData(
         // This is the theme of your application.
         //
