0343b81d2ca1aff147d7655a71e9a3f6f9b9dd03
윤혁진 <gurwk574@naver.com>
2023-07-13 21:13:13 +0900

    first commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Robota'),
+      themeMode: ThemeMode.system,
+      home: const FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("안녕하세요 17조 윤혁진입니다"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://i.ibb.co/XxNytV0/yoon.jpg',
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height * 0.8,
+          ),
+          ElevatedButton(
+            child: const Text("인터뷰 보기"),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("간략한 인터뷰!"),
+      ),
+      body: SafeArea(
+        top: true,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(),
+          child: Image.network(
+            'https://i.ibb.co/cXmyPxW/interview.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
